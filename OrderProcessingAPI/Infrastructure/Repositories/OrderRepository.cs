3ad209d09cd92b5cb6f93ee00d9bb44659a01af9
lucasgls <lucasglsilva7@gmail.com>
2026-06-26 10:09:25 -0300

    feat: add IOrderRepository and OrderRepository

---
+++ OrderProcessingAPI/Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderProcessingAPI.Application.Interfaces;
+using OrderProcessingAPI.Domain.Entities;
+using OrderProcessingAPI.Infrastructure.Data;
+
+namespace OrderProcessingAPI.Infrastructure.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly AppDbContext _context;
+
+        public OrderRepository(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+
+        public async Task<Order?> GetByIdAsync(Guid orderId)
+        {
+            return await _context.Orders
+                .FindAsync(orderId);
+        }
+
+        public async Task<IReadOnlyList<Order?>> GetAllAsync()
+        {
+            return await _context.Orders
+                .ToListAsync();
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
